5e8804b36b79bde831f1874531685c731b7e9bee
LincolnX2 <sabur.yinus@gmail.com>
2022-09-08 16:25:21 -0700

    c-codes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  *main print the string in the put fuction
- *Description: using the main function
+ *description: using the main function
  *this program prints "programming is like building a multilingual puzzle
  *Return: 0
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
-return (0);
+  puts("\"programming is like building a multilingual puzzle");
+  return (0);
 }
