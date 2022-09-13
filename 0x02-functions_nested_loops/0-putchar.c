450b2a984b8d5aaad9183f5b0de213c0d97355bd
LincolnX2 <sabur.yinus@gmail.com>
2022-09-13 06:40:18 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
-* main - Entry point
+* main - print putchar
 *
 * Return: Always 0 (Success)
 */
 
 int main(void)
+  
 {
-char *sh = "main";
-while (*sh)
-{
-_putchar(*sh);
-sh++;
-}
-_putchar("\n");
-return (0);
+_putchar("_")    
+_putchar("p")    
+_putchar("u")    
+_putchar("t")    
+_putchar("c")    
+_putchar("h")    
+_putchar("a")    
+_putchar("r")
+return (0); 
 }
