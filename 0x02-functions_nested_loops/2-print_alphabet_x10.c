51fc6b4ec80ed1802f92d925ba6146eb25e9815d
LincolnX2 <sabur.yinus@gmail.com>
2022-09-13 14:47:19 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,15 @@
  * Return: 0
  */
 
-void print_alphabet_x10(void) 
-{  
+void print_alphabet_x10(void)
+{
 char c, i;
 for (i = 0; i <= 9; i++)
-{    
+{
 for (c = 'a'; c <= 'z'; c++)
-{	
-_putchar(c);	
-}    
-_putchar('\n');    
+{
+_putchar(c);
+}
+_putchar('\n');
 }
 }
