15245fb729aecf4c2cce7537c0aa2289a85b4fa2
LincolnX2 <sabur.yinus@gmail.com>
2022-09-16 09:05:12 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,11 +6,16 @@
  * *Return: 1 if it is, 0 otherwise*
  * */
 
+int _isdigit(int c)
+  
 {
-		if ((c >= 48) && (c <= 57))
+
+ if ((c >= 48) && (c <= 57))
 
  {
 	return (1);
  }	
 	return (0);
+
 }
+
