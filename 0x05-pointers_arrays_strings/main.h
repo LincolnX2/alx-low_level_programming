a520851c85be0722172cd3184b314c45992c14de
LincolnX2 <sabur.yinus@gmail.com>
2022-09-20 10:00:22 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,15 +1,7 @@
 #ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
+#define MAIN_H/
 
-
-/**
-* main - header file for prototypes of all your functions.
-*
-* Return: Always 0.
-*/
-
-int _putchar(char c)
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -20,5 +12,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif /* MAIN_H */
