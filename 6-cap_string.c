3ef6492a820b45583e58bfd654dc86030cbc2ee3
LincolnX2 <sabur.yinus@gmail.com>
2022-09-21 15:26:06 -0700

    6-cap_string.c

--- 6-cap_string.c
+++ 6-cap_string.c
@@ -14,24 +14,19 @@ char *cap_string(char *n)
 
 for (i = 0; n[i] != '\0'; i++)
 	{
-if (n[i] >= 'a' && n[i] <= 'z')
-
-	{
+	if (n[i] >= 'a' && n[i] <= 'z')
+{
 		n[i] = n[i] - cap;
-	}
-
+}
 		cap = 0;
-
 for (x = 0; x <= 12; x++)
-	{
-		if (n[i] == separators[x])
-		{
+{
+	if (n[i] == separators[x])
+{
 		x = 12;
 		cap = 32;
-		}
- 	}
+}
+}
 	}
-
-	return (n);
-
+return (n);
 }
