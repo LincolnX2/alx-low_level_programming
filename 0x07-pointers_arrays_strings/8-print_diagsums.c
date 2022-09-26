47222c3113c4f01fc261c4ec48fe47e5f8963348
LincolnX2 <sabur.yinus@gmail.com>
2022-09-26 16:41:25 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,7 +10,7 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, n, total1 = 0, total2 = 0;
+int i, n, total1 = 0, total2 = 0;
 
 for (i = 0; i <= (size * size); i = i + size + 1)
 			total1 = total1 + a[i];
