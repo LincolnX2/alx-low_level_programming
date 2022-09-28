a01215c435dc73bcf953dd65d81eb0ad6bb7b96c
LincolnX2 <sabur.yinus@gmail.com>
2022-09-28 08:47:50 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,23 +2,21 @@
 
 /**
  * main-a function that print reverse of a passes string.
- * @s-parameter:a pointer to a function
- *
+ * @s:input
  * return 0
  */
-
 void _print_rev_recursion(char *s);
 {
-	if (*s)
-	{
-	_print_rev_recursion;
-	putchar("%c\n",*s);
-	}
-}
-int main()
+if (*s)
 {
- 	char a[] = "code";
-  	_print_rev_recursion(a);
+_print_rev_recursion;
+putchar("%c\n", *s);
+}
+}
+int main(void)
+{
+char a[] = "code";
+_print_rev_recursion(a);
 return (0);
 }
 
