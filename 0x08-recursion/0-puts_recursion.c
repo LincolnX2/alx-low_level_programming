b2807327618e7f9abb5ae2b540bca154a90e29bc
LincolnX2 <sabur.yinus@gmail.com>
2022-09-28 06:52:14 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main-a function that prints a string.
+ * using puts
+ * returns value of puts
+ */
+
+ int puts(const char *s)
+{
+unsigned puts;
+}
+
+int main()
+{
+puts("puts with recursion");
+return (0);
+}
