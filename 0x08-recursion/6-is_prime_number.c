eff536637c622b2ad3e99356b44966886bc4b3ed
LincolnX2 <sabur.yinus@gmail.com>
2022-09-28 16:26:58 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,7 +9,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	else if (prime_checker(n, n / 2) > 0)
+	if (prime_checker(n, n / 2) > 0)
 		return (1);
 		return (0);
 }
@@ -26,6 +26,5 @@ int prime_checker(int n, int i)
 		return (1);
 	if (n % i == 0)
 		return (0);
-	else
 		return (prime_checker(n, i - 1));
 }
