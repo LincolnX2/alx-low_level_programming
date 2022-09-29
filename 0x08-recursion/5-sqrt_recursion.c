c3d45cd0407c57ab45e94134a324705e4a03caa0
LincolnX2 <sabur.yinus@gmail.com>
2022-09-28 17:04:47 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include "stdio.h"
+#include "math.h"
 
 int prime_checker(int n, int i);
 /**
@@ -10,7 +12,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	else if (prime_checker(n, n / 2) > 0)
+	 if (prime_checker(n, n / 2) > 0)
 		return (1);
 	return (0);
 }
@@ -26,6 +28,5 @@ int prime_checker(int n, int i)
 		return (1);
 	if (n % i == 0)
 		return (0);
-else
 		return (prime_checker(n, i - 1));
 }
