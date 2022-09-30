6af2270e05e7b4b363d64321d0a3ac58f43d94bc
LincolnX2 <sabur.yinus@gmail.com>
2022-09-30 16:40:38 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,18 +12,18 @@ int main(int argc, char *argv[])
 {
 	int i, res = 1;
 
-		if (argc != 3)
+if (argc != 3)
+{
+	printf("%s\n", "Error");
+	return (1);
+}
+else
 			{
-			printf("%s\n", "Error");
-				return (1);
+for (i = 1; i < argc; i++)
+			{
+	res *= atoi(argv[i]);
 			}
-		else
-			{
-		for (i = 1; i < argc; i++)
-			{
-																res *= atoi(argv[i]);
-																		}
 			printf("%d\n", res);
 			}
-		return (0);
+	return (0);
 }
