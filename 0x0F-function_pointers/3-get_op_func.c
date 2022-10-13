c8f67dedb793780d4ec808e0e2b58e007c230927
LincolnX2 <sabur.yinus@gmail.com>
2022-10-13 15:08:13 -0700

    3-main.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,13 +18,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
+int i = 0;
 
-	int i = 0;
-	while (op_s[i].op)
+while (op_s[i].op)
 	{
-	if (*(op_s[i].op) == *s)
-		return (op_s[i].f);
-		i++;
+if (*(op_s[i].op) == *s)
+return (op_s[i].f);
+i++;
 	}
 	return (NULL);
 }
