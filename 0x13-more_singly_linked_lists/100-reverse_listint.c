1895e1e8a4faea09bd54efea6eb159eae6a38987
LincolnX2 <sabur.yinus@gmail.com>
2022-10-25 14:44:34 -0700

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -13,43 +13,21 @@
 listint_t *reverse_listint(listint_t **head)
 
 {
-
 	listint_t *copy;
-
 	listint_t *temp;
 
-
-
 	if (*head == NULL)
-
 		return (NULL);
 
-
-
 	copy = *head;
-
 	temp = NULL;
-
-
-
 	while (*head != NULL)
-
 	{
-
 		copy = (*head)->next;
-
 		(*head)->next = temp;
-
 		temp = *head;
-
 		*head = copy;
-
 	}
-
-
-
 	*head = temp;
-
 	return (*head);
-
 }
