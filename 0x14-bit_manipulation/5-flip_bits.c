3be1c50c4c80568700cc93b6cc923c7a6ec8c832
LincolnX2 <sabur.yinus@gmail.com>
2022-10-28 04:07:52 -0700

    aa

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,16 +9,14 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int count = 0;
+unsigned int count = 0;
 
-	while (n != 0 || m != 0)
+while (n != 0 || m != 0)
 	{
-		if ((n & 1) != (m & 1))
-			count++;
+	if ((n & 1) != (m & 1))
+		count++;
 		m = m >> 1;
 		n = n >> 1;
 	}
-
 	return (count);
-
 }
