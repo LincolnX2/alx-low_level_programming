fa5b1c0963b57dc63afc6de83cf311ae8323650f
LincolnX2 <sabur.yinus@gmail.com>
2023-02-23 10:18:03 -0800

    0x1C-makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
-"""island_perimeter module
+"""
+Defines function to find island perimeter
 """
 
 
 def island_perimeter(grid):
-    """Returns perimeter of the grid
     """
+    Finds the perimeter of an island represented by 1s
+    surrounded by 0s in a list of lists of integers
+    """
+    perimeter = 0
+    for rows in range(len(grid)):
+        for columns in range(len(grid[rows])):
+            if grid[rows][columns] == 1:
+                if columns == 0:
+                    perimeter += 1
+                elif grid[rows][columns - 1] == 0:
+                    perimeter += 1
+                if columns == (len(grid[rows]) - 1):
+                    perimeter += 1
+                elif grid[rows][columns + 1] == 0:
+                    perimeter += 1
+                if rows == 0:
+                    perimeter += 1
+                elif grid[rows - 1][columns] == 0:
+                    perimeter += 1
+                if rows == (len(grid) - 1):
+                    perimeter += 1
+                elif grid[rows + 1][columns] == 0:
+                    perimeter += 1
+    return perimeter
